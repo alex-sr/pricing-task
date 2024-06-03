e6b527c477de2966a90ca71153a34faef85fae6b
Petrik <112963523+petrikpopov@users.noreply.github.com>
2024-06-03 15:14:20 +0300

    Del border.

--- js/selectData.js
+++ js/selectData.js
@@ -1,26 +1,28 @@
 export const selectData = [
   {
     value: 'logisticsExplorer',
-    label: '<img src="img/icons/le icon.svg" alt="Icon-1" style="width: 24px; height: 24px;"><p>Logistics Explorer</p>',
+    label:
+      '<img src="img/icons/explorer.svg" alt="Icon-1" style="width: 24px; height: 24px;"><p>Logistics Explorer</p>',
   },
   {
     value: 'trackingSystem',
-    label: '<img src="img/icons/le-icon-1.svg" alt="Icon-2" style="width: 24px; height: 24px;"><p>Tracking System</p>',
+    label: '<img src="img/icons/tracking.svg" alt="Icon-2" style="width: 24px; height: 24px;"><p>Tracking System</p>',
   },
   {
     value: 'shipSchedule',
-    label: '<img src="img/icons/le-icon-2.svg" alt="Icon-3" style="width: 24px; height: 24px;"><p>Ship Shedules</p>',
+    label: '<img src="img/icons/schedule.svg" alt="Icon-3" style="width: 24px; height: 24px;"><p>Ship Shedules</p>',
   },
   {
     value: 'loadCalculator',
-    label: '<img src="img/icons/le-icon-3.svg" alt="Icon-4" style="width: 24px; height: 24px;"><p>Load Сalculator</p>',
+    label: '<img src="img/icons/calc.svg" alt="Icon-4" style="width: 24px; height: 24px;"><p>Load Сalculator</p>',
   },
   {
     value: 'distanceTime',
-    label: '<img src="img/icons/le-icon-4.svg" alt="Icon-5" style="width: 24px; height: 24px;"><p>Distance & Time</p>',
+    label: '<img src="img/icons/distance.svg" alt="Icon-5" style="width: 24px; height: 24px;"><p>Distance & Time</p>',
   },
   {
     value: 'dfaMembership',
-    label: '<img src="img/icons/Membership.svg" alt="Icon-5" style="width: 24px; height: 24px;"><p>DFA Membership</p>',
+    label:
+      '<img src="img/icons/dfa-membership.svg" alt="Icon-5" style="width: 24px; height: 24px;"><p>DFA Membership</p>',
   },
 ];
